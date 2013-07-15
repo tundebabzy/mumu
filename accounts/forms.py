7a05dbf7144e7a794ca60e74d99a2077f1863547
Tunde Akinyanmi <tundebabzy@gmail.com>
2013-07-15 12:46:17 +0100

    changed the display widget to RadioSelect

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,8 +12,10 @@ class PurchaseLevelForm(forms.ModelForm):
     class Meta:
         model = Payment
         exclude = ['user', 'paper', 'time', 'has_used_free']
+        widgets = {'level': forms.RadioSelect,}
         
 class PurchasePaperForm(forms.ModelForm):
     class Meta:
         model = Payment
         exclude = ['user', 'level', 'time', 'has_used_free']
+        widgets = {'paper': forms.RadioSelect,}
