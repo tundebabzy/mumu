e84d9763e7cad40a5441d994b26ce22f690d8388
Tunde Akinyanmi <tundebabzy@gmail.com>
2013-07-20 17:04:23 +0100

    pass Payment object in request directly to template to reduce redundant query

--- quizzer/templatetags/quiz_listing.py
+++ quizzer/templatetags/quiz_listing.py
@@ -8,22 +8,16 @@ import datetime
 register = template.Library()
 
 class NavigationNode(template.Node):
-    def __init__(self, user_obj):
+    def __init__(self, user_obj, payment_obj):
         self.user_obj = template.Variable(user_obj)
+        self.payment_obj = template.Variable(payment_obj)
         
     def get_user_from_node(self, context):
-        user = context.render_context[self].resolve(context)
-        return user
+        return context.render_context[self]['user'].resolve(context)
         
-    def get_user_last_payment(self, user):
-        try:
-            tz = get_current_timezone()
-            now = datetime.datetime.now()
-            last_payment = user.payment_set.all().order_by('-effective_time').filter(effective_time__lte=make_aware(now, tz))[0]
-        except IndexError:
-            return None
-        return last_payment
-        
+    def get_user_last_payment(self, context):
+        return context.render_context[self]['payment_obj'].resolve(context)
+
     def _gen_html(self, tag_type, obj):
         return u"""
                 <%s><a href="%s">%s</a><%s>
@@ -50,7 +44,7 @@ class NavigationNode(template.Node):
 
     def make_html(self, context):
         user = self.get_user_from_node(context)
-        last_payment = self.get_user_last_payment(user)
+        last_payment = self.get_user_last_payment(context)
         
         if not user.is_staff and last_payment is None:
             return self._error_html()
@@ -94,7 +88,10 @@ class NavigationNode(template.Node):
     def render(self, context):
         if not self in context.render_context:
             # For the first time the node is rendered in the template
-            context.render_context[self] = self.user_obj
+            context.render_context[self] = {
+                'user': self.user_obj,
+                'payment_obj': self.payment_obj
+            }
         try:
             return self.make_html(context)
         except:
@@ -103,7 +100,7 @@ class NavigationNode(template.Node):
 @register.tag(name="quiz_listing")
 def get_categories(parser, token):
     try:
-        tag_name, user_obj = token.split_contents()
+        tag_name, user_obj, payment_obj = token.split_contents()
     except ValueError:
         raise template.TemplateSyntaxError('tag takes exactly one argument')
-    return NavigationNode(user_obj)
+    return NavigationNode(user_obj, payment_obj)
