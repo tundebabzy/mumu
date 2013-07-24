3063044480fb540032c167d72303fd62d5b71213
Tunde Akinyanmi <tundebabzy@gmail.com>
2013-07-24 09:39:24 +0100

    bugfix: get_queryset for staff changed else would put links that are unreachable

--- quizzer/views/flashcards.py
+++ quizzer/views/flashcards.py
@@ -21,18 +21,16 @@ class GenerateFlashCardView(DetailView, SessionMixin):
         return super(GenerateFlashCardView, self).get(request, *args, **kwargs)
 
     def query_database(self):
-        TIMES = 1
         topic_slug = self.kwargs.get('topic_slug', None)
         max_ = self.model.objects.aggregate(models.Max('id'))['id__max']
-        i = 0
-        while i < TIMES:
+        while 1:
             try:
                 random_pk = randint(1, max_)
                 result = self.model.objects.filter(pk__range=(random_pk,
                     random_pk + 10), topic__slug=topic_slug)[0]
                 self.set_session_var('topic_slug', topic_slug)
                 return result
-            except self.model.DoesNotExist:
+            except IndexError:
                 pass
 
     def get_object(self, queryset=None):
@@ -60,13 +58,13 @@ class FlashCardListView(ListView):
     
     def get_queryset(self):
         if self.request.user.is_staff:
-            queryset = Topic.objects.all()
+            qs = FlashCard.objects.all()
         elif self.request.status.has_not_expired():
             if self.request.status.get_category_paid_for() == 'paper':
                 qs = FlashCard.objects.filter(paper=self.request.status.paper)
             elif self.request.status.get_category_paid_for() == 'level':
                 qs = FlashCard.objects.filter(level=self.request.status.level)
-            qs = qs.order_by('topic').distinct('topic')
-            qs = qs.values_list('topic_id', flat=True)
-            queryset = Topic.objects.filter(id__in=qs)
+        qs = qs.order_by('topic').distinct('topic')
+        qs = qs.values_list('topic_id', flat=True)
+        queryset = Topic.objects.filter(id__in=qs)
         return queryset
