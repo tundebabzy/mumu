d87517720763fe205f323ef556217199a844f0e6
Tunde Akinyanmi <tundebabzy@gmail.com>
2013-12-19 10:43:05 +0100

    updated url tags for django 1.5 compatibility

---
+++ quizzer/views/backend.py
@@ -0,0 +1,5 @@
+from registration.backends.default.views import RegistrationView
+from quizzer.forms import QuizzerForm
+
+class QuizzerRegistrationView(RegistrationView):
+    form_class = QuizzerForm
