9e9407470732325baf7b6f5c4e2941a68e7d8f6f
Tunde Akinyanmi <tundebabzy@gmail.com>
2014-02-07 08:24:39 +0100

    new decoded utility to help determine categorization of flashcards and question from its code

---
+++ utils/decoder.py
@@ -0,0 +1,127 @@
+# Constants for ExamCodeDecoder class.
+# When there needs to be a change in any of the exam categorisation, it
+# should be done here not in the ExamCodeDecoder class
+########################################################################
+
+EXAM_CODE = {'1': 'ICAN'}
+LEVEL_CODE = {
+    '01': 'FOUNDATION', '02': 'INTERMEDIATE'
+}
+PAPER_CODE = {
+    '01': 'FUNDAMENTALS OF FINANCIAL ACCOUNTING',
+    '02': 'AUDITING AND ASSURANCE',
+}
+TOPIC_CODE = {
+    '001': 'ACCOUNTING FRAMEWORK', '002': 'BASIC ACCOUNTING STANDARDS',
+    '003': 'FUNDAMENTALS AND PRINCIPLES OF AUDIT'
+}
+
+########################################################################
+
+class ExamCodeDecoder:
+    """
+    This is a class that decodes exam codes and returns the
+    categorisation for that code. It is meant to be used with Question
+    objects.
+
+    The codes are 8 character codes.
+
+    This is how the code is analysed:
+    *   The first digit represents the EXAM e.g ICAN. Only numerals are
+        allowed.
+    *   The next two digits represent the LEVEL e.g Foundation,
+        Intermediate. Only numerals are allowed
+    *   The next two digits represent the PAPER e.g Auditing and Assurance.
+        It is alphanumeric.
+    *   The next three digits represent the TOPIC. It is alphanumeric
+
+    The decoder uses this information to determine what EXAM, LEVEL,
+    PAPER and TOPIC.
+    """
+    def __init__(self):
+        self.exam_codes = EXAM_CODE
+        self.level_codes = LEVEL_CODE
+        self.paper_codes = PAPER_CODE
+        self.topic_codes = TOPIC_CODE
+        self.decoded = {}
+
+    def decode(self, code):
+        """
+        Tries to analyse a code and return a dictionary of the
+        categorisations.
+        Code *MUST* be 8 character unicode string.
+        Whenever *ANY* failure occurs it returns an empty dictionary.
+        """
+        if isinstance(code, unicode) and len(code) == 8:
+            self.decoded['exam'] = self.exam_codes.get(code[0], None)
+            self.decoded['level'] = self.level_codes.get(code[1:3], None)
+            self.decoded['paper'] = self.paper_codes.get(code[3:5], None)
+            self.decoded['topic'] = self.topic_codes.get(code[5:8], None)
+
+        if None in self.decoded.values():
+            self.decoded = {}
+
+        return self.decoded
+        
+    def translate_code(self, code, category):
+        """
+        Analyse code and return the category it represents.
+        Returns `str`
+        """
+        decoded = self.decode(code)
+        return decoded.get(category, 'FAILED')
+
+    def get_category_code(self, code, category):
+        relevant_code = ''
+
+        if category == 'exam' and code[0] in self.exam_codes:
+            relevant_code = code[0]
+        elif category == 'level' and code[1:3] in self.level_codes:
+            relevant_code = code[1:3]
+        elif category == 'paper' and code[3:5] in self.paper_codes:
+            relevant_code = code[3:5]
+        elif category == 'topic' and code[5:8] in self.topic_codes:
+            relevant_code = code[5:8]
+
+        return relevant_code
+
+    def translate_sub_code(self, code, category):
+        translation = None
+        
+        if category == 'exam' and code in self.exam_codes:
+            translation = self.exam_codes[code]
+        elif category == 'level' and code in self.level_codes:
+            translation = self.level_codes[code]
+        elif category == 'paper' and code in self.paper_codes:
+            translation = self.paper_codes[code]
+        elif category == 'topic' and code in self.topic_codes:
+            translation = self.topic_codes[code]
+
+        return translation
+
+    def get_code_list(self, code, code_list, category):
+        """
+        code: sub-code
+        code_list: list of complete codes
+        category: ['exam', 'level', 'paper', 'topic']
+
+        Because exam code is at the beginning of the code and topic code
+        is at the end of the complete code, exam and topic categories
+        are not recommended here
+        """
+        import itertools
+        iterator = None
+        final_list = []
+        
+        if category == 'exam':
+            iterator = itertools.ifilter(lambda x: x[0] == code, code_list)
+        elif category == 'level':
+            iterator = itertools.ifilter(lambda x: x[1:3] == code, code_list)
+        elif category == 'paper':
+            iterator = itertools.ifilter(lambda x:x[3:5] == code, code_list)
+        elif category == 'topic':
+            iterator = itertools.ifilter(lambda x:x[5:8] == code, code_list)
+
+        final_list = list(iterator)
+
+        return final_list
