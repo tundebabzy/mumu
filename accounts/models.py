560fce2d8b4dd6f45ca79b51a26b679fedc7a975
Tunde Akinyanmi <tundebabzy@gmail.com>
2014-02-10 13:16:18 +0100

    removed unused imports - re, render_to_string, hashlib, etc

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,6 @@
-import hashlib
-import random
-import re
+import registration.models
 
-from registration.models import RegistrationProfile
-
-from django.contrib.auth.models import User
 from django.db import models
-from django.db import transaction
-from django.utils.translation import ugettext_lazy as _
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.conf import settings
 
 class ContractType(models.Model):
     contract_type = models.CharField(max_length=10)
@@ -32,19 +22,19 @@ class Department(models.Model):
         return self.name
 
 class Manager(models.Model):
-    staff = models.ForeignKey(RegistrationProfile, null=True, blank=True)
+    staff = models.ForeignKey(registration.models.RegistrationProfile, null=True, blank=True)
 
     def __unicode__(self):
         return self.staff.user.__unicode__()
     
 class Editor(models.Model):
-    staff = models.ForeignKey(RegistrationProfile, null=True, blank=True)
+    staff = models.ForeignKey(registration.models.RegistrationProfile, null=True, blank=True)
 
     def __unicode__(self):
         return self.staff.user.__unicode__()
 
 class Researcher(models.Model):
-    staff = models.ForeignKey(RegistrationProfile, null=True, blank=True)
+    staff = models.ForeignKey(registration.models.RegistrationProfile, null=True, blank=True)
 
     def __unicode__(self):
         return self.staff.user.__unicode__()
