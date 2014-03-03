9da33d280e96954b0e40c495733da1fe11797410
Tunde Akinyanmi <tundebabzy@gmail.com>
2014-03-03 14:49:37 +0100

    changed paginate_by to 10

--- mumublog/urls.py
+++ mumublog/urls.py
@@ -12,7 +12,7 @@ urlpatterns = patterns('',
 
                        url('^page/(?P<page>[0-9]+)/$', ListView.as_view(
                            template_name='bloglist.html', queryset=Article.objects.filter(approved=True),
-                           http_method_names=['get'], paginate_by=2),
+                           http_method_names=['get'], paginate_by=15),
                            name='article_list_paged'),
 
                        url('^(?P<slug>[a-zA-Z0-9-]+)/$', DetailView.as_view(template_name='article.html',
