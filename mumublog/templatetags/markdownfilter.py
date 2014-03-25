f3863eb94ef115646c60ded79bea19cceb274e58
Tunde Akinyanmi <tundebabzy@gmail.com>
2014-03-25 09:07:54 +0100

    Forgot to add markdown filter to open ended answers page. added sane_list extension to the markdown engine

--- mumublog/templatetags/markdownfilter.py
+++ mumublog/templatetags/markdownfilter.py
@@ -11,5 +11,5 @@ def convert_markdown(text):
     Ideally, there should be sanitizers but considering the fact that the admin is going to be open to only trusted
     people, i'm going to give the benefit of a doubt because bleach is not fast.
     """
-    return markdown.markdown(text, extensions=['tables'])
+    return markdown.markdown(text, extensions=['tables', 'sane_lists'])
 
