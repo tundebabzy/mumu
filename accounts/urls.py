ad05debc6ea7c60d52cddf5167f481cf0758d5c4
Tunde Akinyanmi <tundebabzy@gmail.com>
2014-03-27 12:12:01 +0100

    optimised imports

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,4 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView, ListView
+from django.conf.urls import patterns, url
 
 from accounts.views import UpdateUserNamesView
 
